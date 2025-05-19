157016211c43e0b7d3e81d444f3e648c05794cad
Olga <kartoshkaslukom@gmail.com>
2025-05-19 18:13:48 +0200

    feat: add handling closing server

--- src/ws_server.ts
+++ src/ws_server.ts
@@ -11,14 +11,37 @@ class WsServer {
   }
 
   start() {
+    this.#server.on('listening', () => {
+      console.log('WebSocket server is listening on port 3000');
+    });
+
     this.#server.on('connection', (socket) => {
+      console.log('aaaaaaaa');
       socket.on('message', (message) => {
         console.log(message.toString());
         this.#controller.chooseAction(message, socket);
       });
 
       socket.on('close', () => {
-        console.log('Клиент отключился');
+        console.log('Client disconnected');
+      });
+    });
+
+    this.#server.on('close', () => {
+        console.log('WebSocket server closed');
+    });
+
+    process.on('SIGINT', () => {
+      console.log("SIGINT received");
+      this.#server.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.close(1001, 'Server is shutting down');
+        }
+      });
+
+      this.#server.close(() => {
+        console.log('WebSocket server closed');
+        process.exit(0);
       });
     });
   }
